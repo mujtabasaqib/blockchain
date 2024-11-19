5367ec7d309cf5fd1d8dd8bbfbe5c668bb2a426a
mujtabasaqib <mujtabasaqib99@gmail.com>
2024-11-19 20:50:18 +0500

    added transaction,block,chain class

--- index.ts
+++ index.ts
@@ -1,15 +1,50 @@
 import * as  crypto from 'crypto';
 
 class Transaction {
-
+  constructor(
+    public amount: number,
+    public sender: string,
+    public reciever: string
+  ) {}
+  toString(){
+    return JSON.stringify(this)
+  }
 }
 
 class Block {
+  constructor(
+    public prevHash: string,
+    public transaction: Transaction,
+    public timestamp = Date.now()
+  ) {}
 
+  get hash(){
+    const str = JSON.stringify(this);
+    const hash = crypto.createHash('sha256');
+    hash.update(str).end();
+    return hash.digest('hex');
+  }
 }
 
 class Chain {
+  public static instance = new Chain();
+  chain: Block[]
 
+  constructor() {
+    this.chain = [];
+    this.createGenesisBlock();
+    //this.chain = [new Block('', new Transaction(100, 'genesis', 'satoshi'))]
+  }
+
+  createGenesisBlock(){
+    this.chain.push(new Block('', new Transaction(100, 'genesis', 'satoshi')))
+  }
+
+  get lastBlock(){
+    return this.chain[this.chain.length - 1]
+  }
+
+  
 }
 
 class Wallet {
